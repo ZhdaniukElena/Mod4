a197843278a43bd3cdd0847ffd3d912bd5cb499b
Zhdaniuk Olena <e.mssixty@gmail.com>
2018-01-31 09:04:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let arr = [];
+
+
+function addKeyboardLayout(alphabet) {
+    arr.push(alphabet);
+    return arr;
+};
+console.log(addKeyboardLayout('qwertyuiop[]'));
+console.log(addKeyboardLayout('asdfghjkl;\''));
+console.log(addKeyboardLayout('zxcvbnm,./'));
+
+function getRandCharInRow(row) {
+    let min = 0;
+    let max = arr[row].length;
+    let i = Math.floor(Math.random() * (max - min)) + min;
+    return arr[row][i];
+};
+console.log(getRandCharInRow(0));
+
+function getRandCharInAlph() {
+    let arrDublicated = arr.slice();
+    let arrReduced = arrDublicated.reduce( (memo, v)  => {
+        return memo + v;
+    }, "");
+    let min = 0;
+    let max = arrReduced.length;
+    let i = Math.floor(Math.random() * (max - min)) + min;
+    return arrReduced[i];
+};
+console.log(getRandCharInAlph());
